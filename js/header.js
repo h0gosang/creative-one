3802923780c021a9814978e4314d374ab6935267
h0sang <h0gosang1@gmail.com>
2020-04-13 16:08:38 +0300

    Task #7 script hover complete

---
+++ js/header.js
@@ -0,0 +1,73 @@
+var theMenu1 = document.getElementById('menu1'),
+    theMenu2 = document.getElementById('menu2'),
+    theMenu3 = document.getElementById('menu3'),
+    theMenu4 = document.getElementById('menu4'),
+    theMenu5 = document.getElementById('menu5'),
+    theMenu6 = document.getElementById('menu6'),
+    theMenu7 = document.getElementById('menu7'),
+    theMenu8 = document.getElementById('menu8'),
+    theMenu9 = document.getElementById('menu9');
+
+    $('#link2').hover(
+      function() {
+        theMenu2.classList.add('active');
+      }, function() {
+        theMenu2.classList.remove('active');
+      }
+    );
+
+    $('#link3').hover(
+      function() {
+        theMenu3.classList.add('active');
+      }, function() {
+        theMenu3.classList.remove('active');
+      }
+    );
+
+    $('#link4').hover(
+      function() {
+        theMenu4.classList.add('active');
+      }, function() {
+        theMenu4.classList.remove('active');
+      }
+    );
+
+    $('#link5').hover(
+      function() {
+        theMenu5.classList.add('active');
+      }, function() {
+        theMenu5.classList.remove('active');
+      }
+    );
+
+    $('#link6').hover(
+      function() {
+        theMenu6.classList.add('active');
+      }, function() {
+        theMenu6.classList.remove('active');
+      }
+    );
+
+    $('#link7').hover(
+      function() {
+        theMenu7.classList.add('active');
+      }, function() {
+        theMenu7.classList.remove('active');
+      }
+    );
+
+    $('#link8').hover(
+      function() {
+        theMenu8.classList.add('active');
+      }, function() {
+        theMenu8.classList.remove('active');
+      }
+    );
+
+    $('#link9').hover(
+      function() {
+        theMenu9.classList.add('active');
+      }, function() {
+        theMenu9.classList.remove('active');
+      }
+    );
